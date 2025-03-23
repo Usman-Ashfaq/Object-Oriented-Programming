16bd7d8da87d393459c2b520efbce5ccd2a7d0df
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-03-23 23:39:32 +0500

    Update this pointer .cpp

--- this pointer .cpp
+++ this pointer .cpp
@@ -1,19 +1,19 @@
-//#include<iostream>
-//using namespace std;
-//class student
-//{
-//	//int id;
-//	//int no;
-//	int a;
-//public:
-//	//void setdata(int a, int b)
-//	//student & setdata(int a, int b)
-// //   {
-//		//this->id = a;                ////this is areserved key word point to the currrent value
-//		//this->no = b;
-//		//return *this;              //returning a pointer
+#include<iostream>
+using namespace std;
+class student
+{
+	int id;
+int no;
+int a;
+public:
+//	void setdata(int a, int b)
+//	student & setdata(int a, int b)
+//    {
+//		this->id = a;                ////this is areserved key word point to the currrent value
+//		this->no = b;
+//		return *this;              //returning a pointer
 //
-//	//}
+//	}
 //	void setdata(int a)
 //	{
 //		this->a = a;
